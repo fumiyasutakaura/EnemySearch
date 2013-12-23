143f53d96a27d7f1d470e561c304d2065bd70cc2
fumiyasutakaura <master@royal-gadget.co.jp>
2013-12-23 14:34:38 +0900

    added project

---
+++ EnemySearch/EnemySearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EnemySearch
+//
+//  Created by Fumiyasu Takaura on 12/23/13.
+//  Copyright (c) 2013 Fumiyasu Takaura. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ EnemySearch/EnemySearch/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EnemySearch
+//
+//  Created by Fumiyasu Takaura on 12/23/13.
+//  Copyright (c) 2013 Fumiyasu Takaura. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
