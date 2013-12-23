8d4c4e32e639c4d684a61baa958e56a59f16ea99
fumiyasutakaura <master@royal-gadget.co.jp>
2013-12-23 14:38:21 +0900

    added Test hpp cpp

---
+++ EnemySearch/src/Test.cpp
@@ -0,0 +1,9 @@
+//
+//  Test.cpp
+//  EnemySearch
+//
+//  Created by Fumiyasu Takaura on 12/23/13.
+//  Copyright (c) 2013 Fumiyasu Takaura. All rights reserved.
+//
+
+#include "Test.hpp"
---
+++ EnemySearch/src/Test.hpp
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  EnemySearch
+//
+//  Created by Fumiyasu Takaura on 12/23/13.
+//  Copyright (c) 2013 Fumiyasu Takaura. All rights reserved.
+//
+
+#ifndef __EnemySearch__Test__
+#define __EnemySearch__Test__
+
+#include <iostream>
+
+#endif /* defined(__EnemySearch__Test__) */
